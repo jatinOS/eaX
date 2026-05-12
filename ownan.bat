7e4c321fc95422149b0575a5fe13497c84263d00
jatinOS <sharmagajender095@gmail.com>
2026-05-12 17:36:10 +0530

    Create ownan.bat with OS information
    Add comments about the OS and its development.

---
+++ ownan.bat
@@ -0,0 +1,11 @@
+@REM  this OS is made by jatin and jatin labs
+@REM  our cmp name is jatiN corp...
+@REM  OS name is eaX 
+@REM  its made for programming gaming 
+@REM kernel is made by jatin labs
+@REM and the files like mk sh Id dockerfile is made by jatinthelord
+@REM CEO tells idea
+@REM NHP DEVS solves problem and json programming language work
+@REM  Jatinthelord , nhpdevs , jatin labs do very very well 
+@REM it tooks 6 months its crazy
+@REM feedback us email = jatinthelord@gmail.com .
